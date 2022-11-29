acbf95bc1990c909c5bf85d77d150d26dcc66dea
CCherry7 <2405693142@qq.com>
2022-11-29 22:58:46 +0800

    feat:  work begin and complete

--- packages/react-reconciler/src/beginWork.ts
+++ packages/react-reconciler/src/beginWork.ts
@@ -1,11 +1,60 @@
 // DFS 递归 递阶段
 
+import { ReactElementType } from "shared/ReactTypes";
+import { mountChildFibers, reconcileChildFibers } from "./ChildReconciler";
 import { FiberNode } from "./fiber";
+import { processUpdateQueue, UpdateQueue } from "./updateQueue";
+import { HostComponent, HostRoot, HostText } from "./workTags";
 
 
-export const beginWork = (fiber: FiberNode) => {
-
+export const beginWork = (wip: FiberNode) => {
+  switch (wip.tag) {
+    case HostRoot:
+      return updateHostRoot(wip)
+    case HostComponent:
+      return updateHostComponent(wip)
+    case HostText:
+      return null
+    default:
+      if (__DEV__) {
+        console.warn(`beginWork ${wip.tag} 未匹配到`);
+      }
+      break;
+  }
 
   return null
   // 返回之fiberNode
 }
+
+
+function updateHostRoot(wip: FiberNode) {
+  const baseState = wip.memoizedState
+  const updateQueue = wip.updateQueue as UpdateQueue<Element>
+  const pending = updateQueue.shared.pending
+  updateQueue.shared.pending = null
+  const { memoizeState } = processUpdateQueue(baseState, pending)
+  wip.memoizedState = memoizeState
+  // 
+  const nextChildren = wip.memoizedState
+  reconcileChildren(wip, nextChildren)
+  return wip
+}
+
+function updateHostComponent(wip: FiberNode) {
+  const nextProps = wip.pendingProps
+  const nextChildren = nextProps.children
+  reconcileChildren(wip, nextChildren)
+  return null
+}
+
+function reconcileChildren(wip: FiberNode, children: ReactElementType | null) {
+  const current = wip.alternate
+  if (current !== null) {
+    // update
+    wip.child = reconcileChildFibers(wip, current?.child, children)
+  } else {
+    // mount
+    wip.child = mountChildFibers(wip, null, children)
+  }
+}
+
--- packages/react-reconciler/src/completeWork.ts
+++ packages/react-reconciler/src/completeWork.ts
@@ -1,7 +1,83 @@
+import { appendInitialChild, Container, createInstance, createTextInstance } from "hostConfig";
 import { FiberNode } from "./fiber";
+import { NoFlags } from "./fiberFlags";
+import { HostComponent, HostRoot, HostText } from "./workTags";
 
 // DFS 递归 归阶段
-export const completeWork = (fiber: FiberNode) => {
+export const completeWork = (wip: FiberNode) => {
+  const newProps = wip.pendingProps
+  const current = wip.alternate
+  switch (wip.type) {
+    case HostComponent:
 
+      if (current !== null && wip.stateNode) {
+        // update
+      } else {
+        // mount
+        // 1.构建dom 
+        const instance = createInstance(wip.type, newProps)
+        appendAllChildren(instance, wip)
+        wip.stateNode = instance
+        // 2.插入dom
+      }
+      bubblePropser(wip)
+      return null
+    case HostText:
+
+      if (current !== null && wip.stateNode) {
+        // update
+      } else {
+        // mount
+        // 1.构建dom 
+        const instance = createTextInstance(newProps.content)
+        wip.stateNode = instance
+        // 2.插入dom
+      }
+      bubblePropser(wip)
+      return null
+    case HostRoot:
+      bubblePropser(wip)
+      return null
+    default:
+      break;
+  }
   return null
+}
+
+function appendAllChildren(parent: Container, wip: FiberNode) {
+  let node = wip.child
+  while (node !== null) {
+    if (node?.tag === HostComponent || node?.tag === HostText) {
+      appendInitialChild(parent, node.stateNode)
+    } else if (node.child !== null) {
+      node.child.return = node
+      node = node.child
+      continue
+    }
+
+    if (node === wip) {
+      return
+    }
+    while (node.sibling === null) {
+      if (node.return === null || node.return === wip) {
+        return
+      }
+      node = node?.return
+    }
+    node.sibling.return = node.return
+    node = node.sibling
+  }
+}
+
+
+function bubblePropser(wip: FiberNode) {
+  let subTreeFlags = NoFlags
+  let child = wip.child
+  if (child !== null) {
+    subTreeFlags |= child.subTreeFlags
+    subTreeFlags |= child.flags
+    child.return = wip
+    child = child.sibling
+  }
+  wip.subTreeFlags |= subTreeFlags
 }
